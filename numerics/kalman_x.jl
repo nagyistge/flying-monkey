79faa6aca553c7bd8969cc07da96d93eb0c25472
waTeim <truthset@gmail.com>
2016-03-20 19:28:32 +0000

    fix kalman_x call

--- numerics/kalman_x.jl
+++ numerics/kalman_x.jl
@@ -5,7 +5,7 @@ s = kalman.initialGuess([0.0,0.0,0.0],[0.1,0.1,0.1]);
 m = kalman.newModel("_",0.1,0.1,0);
 p = kalman.predict("_",m,s);
 
-kalman.update("_",m,p,[0.0,0.0,0.0],1000);
+kalman.update("_",m,p,[0.0,0.0,0.0],0.5);
 kalman.extractMeanFromState(s);
 kalman.extractVarianceFromState(s);
 
