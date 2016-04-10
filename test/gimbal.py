83df7ee36cc9f5cd7a26d83d4d4b64482e223fcd
waTeim <truthset@gmail.com>
2016-04-10 06:58:25 +0000

    tested gimbal test

--- test/gimbal.py
+++ test/gimbal.py
@@ -2,12 +2,16 @@
 
 import dronekit
 import sys
+import time
 
 # Connect to UDP endpoint (and wait for default attributes to accumulate)
 def main():
    target = "udpin:0.0.0.0:14550"
    vehicle = dronekit.connect(target,wait_ready=True)
-   vehicle.gimbal.rotate(-90,0,0)
+   time.sleep(2)
+   vehicle.gimbal.rotate(0,0,0)
+   time.sleep(2)
    vehicle.close()
+   time.sleep(2)
 
 main()
